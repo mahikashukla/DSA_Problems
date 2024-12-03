2aee87198d0852243e6b5c43777322395610fba8
Mahika Shukla <166138877+mahikashukla@users.noreply.github.com>
2024-12-03 21:24:19 +0530

    Create searchInsertPosition.java

---
+++ searchInsertPosition.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int k;
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target){
+                low = mid+1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
